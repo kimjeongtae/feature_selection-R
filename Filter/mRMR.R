dac8ece70aa4c95036d1de7d903de47659e07461
Jeongtae Kim <36652066+kimjeongtae@users.noreply.github.com>
2018-04-21 12:41:54 +0900

    Filter

--- Filter/mRMR.R
+++ Filter/mRMR.R
@@ -1,16 +1,15 @@
-FCBF <- function(X,Y,a=0){
-    su_ic <- SU(X,Y)
-    su_ic <- sort(subset(su_ic, su_ic > a), de=T)
-    L <- names(su_ic)[-1]
-    S <- names(su_ic)[1]
-    i <- 1
-    repeat{
-        if( length(L) == 0 ) break
-        su_ij <- SU(X[L], X[S[i]])
-        L <- subset(L,(su_ic[L] - su_ij) > 0 )
-	  S <- c(S,L[1])
-        L <- L[-1]     
-        i <- i + 1
-    }
-    S[complete.cases(S)]
+mRMR <- function(X,Y,k = 10){
+	ig_ic <- IG(X,Y)
+	m <- which.max(ig_ic)
+	S <- names(ig_ic)[m]
+	L <- names(ig_ic)[-m]
+	ig_ij <- matrix(IG(X[L],X[S]),byrow=T)
+	rownames(ig_ij) <- L
+	while(length(S) < k){
+		m <- which.max(ig_ic[L] - apply(ig_ij,1,mean))
+		S <- c(L[m],S)
+		L <- L[-m]
+		ig_ij <- cbind(ig_ij[L,,drop=F],IG(X[L],X[S[1]]))
+	}
+	rev(S)
 }
